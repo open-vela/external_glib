aa387f1938f385de188fad5dfedc0b855d522f48
openvela-robot <openvela-robot@xiaomi.com>
2019-09-02 06:56:08 +0800

    gdbus-codegen: emit GUnixFDLists if an arg has type 'h'
    Previously, if a method was not annotated with org.gtk.GDBus.C.UnixFD
    then the generated code would never contain GUnixFDList parameters, even
    if the method has 'h' (file descriptor) parameters. However, in this
    case, the generated code is essentially useless: the method cannot be
    called or handled except in degenerate cases where the file descriptors
    are missing or ignored.
    
    Check the argument types for 'h', and if present, generate code as if
    org.gtk.GDBus.C.UnixFD annotation were specified.
    
    This change will break any existing code which refers to the (useless)
    wrappers for such methods. The workaround for such code is to add the
    org.gtk.GDBus.C.UnixFD annotation, which will cause the same generated
    code to be emitted before and after this change.
    
    If this is found to cause widespread problems, we can explore a
    different approach (perhaps emitting a warning from the code generator,
    or annotating the symbols as deprecated).
    
    https://gitlab.gnome.org/GNOME/glib/issues/1726

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -2,21 +2,10 @@
 
 set +e
 
-case "$1" in
-  --log-file)
-    log_file="$2"
-    shift
-    shift
-    ;;
-  *)
-    log_file="_build/meson-logs/testlog.json"
-esac
-
 meson test \
         -C _build \
         --timeout-multiplier ${MESON_TEST_TIMEOUT_MULTIPLIER} \
-        --no-suite flaky \
-        "$@"
+        --no-suite flaky
 
 exit_code=$?
 
@@ -24,6 +13,6 @@ python3 .gitlab-ci/meson-junit-report.py \
         --project-name=glib \
         --job-id "${CI_JOB_NAME}" \
         --output "_build/${CI_JOB_NAME}-report.xml" \
-        "${log_file}"
+        _build/meson-logs/testlog.json
 
 exit $exit_code
