ac053d6fbfa0b1ca1032ef201630b3f0ed8cc717
liujinye <liujinye@xiaomi.com>
2019-10-09 12:33:00 +0800

    Merge branch 'android-warning-fixes' into 'master'
    Fix various compiler warnings on Android
    
    See merge request GNOME/glib!1157

--- glib/tests/atomic.c
+++ glib/tests/atomic.c
@@ -80,9 +80,6 @@ test_types (void)
   g_atomic_pointer_set (&vp, 0);
   vp2 = g_atomic_pointer_get (&vp);
   g_assert_true (vp2 == 0);
-  res = g_atomic_pointer_compare_and_exchange (&vp, &s, &s);
-  g_assert_false (res);
-  g_assert_true (vp == 0);
   res = g_atomic_pointer_compare_and_exchange (&vp, 0, 0);
   g_assert_true (res);
   g_assert_true (vp == 0);
@@ -188,9 +185,6 @@ G_GNUC_END_IGNORE_DEPRECATIONS
   g_atomic_pointer_set (&vp, 0);
   vp2 = g_atomic_pointer_get (&vp);
   g_assert_true (vp2 == 0);
-  res = g_atomic_pointer_compare_and_exchange (&vp, &s, &s);
-  g_assert_false (res);
-  g_assert_true (vp == 0);
   res = g_atomic_pointer_compare_and_exchange (&vp, 0, 0);
   g_assert_true (res);
   g_assert_true (vp == 0);
