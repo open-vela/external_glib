c0751462cf8b5167ca230b53eb124ce6a512b9f0
openvela-robot <openvela-robot@xiaomi.com>
2019-10-14 12:12:06 +0800

    Merge branch 'osinfo' into 'master'
    Add Windows support to g_get_os_info()
    
    See merge request GNOME/glib!1160

--- gio/glocalfilemonitor.c
+++ gio/glocalfilemonitor.c
@@ -46,7 +46,7 @@ struct _GFileMonitorSource {
   GSource       source;
 
   GMutex        lock;
-  GWeakRef      instance_ref;
+  gpointer      instance;
   GFileMonitorFlags flags;
   gchar        *dirname;
   gchar        *basename;
@@ -348,7 +348,6 @@ g_file_monitor_source_handle_event (GFileMonitorSource *fms,
                                     gint64              event_time)
 {
   gboolean interesting = TRUE;
-  GFileMonitor *instance = NULL;
 
   g_assert (!child || is_basename (child));
   g_assert (!rename_to || is_basename (rename_to));
@@ -360,8 +359,7 @@ g_file_monitor_source_handle_event (GFileMonitorSource *fms,
   g_mutex_lock (&fms->lock);
 
   /* monitor is already gone -- don't bother */
-  instance = g_weak_ref_get (&fms->instance_ref);
-  if (instance == NULL)
+  if (!fms->instance)
     {
       g_mutex_unlock (&fms->lock);
       return TRUE;
@@ -452,7 +450,6 @@ g_file_monitor_source_handle_event (GFileMonitorSource *fms,
   g_file_monitor_source_update_ready_time (fms);
 
   g_mutex_unlock (&fms->lock);
-  g_clear_object (&instance);
 
   return interesting;
 }
@@ -503,11 +500,9 @@ g_file_monitor_source_dispatch (GSource     *source,
   QueuedEvent *event;
   GQueue event_queue;
   gint64 now;
-  GFileMonitor *instance = NULL;
 
   /* make sure the monitor still exists */
-  instance = g_weak_ref_get (&fms->instance_ref);
-  if (instance == NULL)
+  if (!fms->instance)
     return FALSE;
 
   now = g_source_get_time (source);
@@ -555,18 +550,15 @@ g_file_monitor_source_dispatch (GSource     *source,
 
   g_file_monitor_source_update_ready_time (fms);
 
-  g_clear_object (&instance);
   g_mutex_unlock (&fms->lock);
 
   /* We now have our list of events to deliver */
   while ((event = g_queue_pop_head (&event_queue)))
     {
       /* an event handler could destroy 'instance', so check each time */
-      instance = g_weak_ref_get (&fms->instance_ref);
-      if (instance != NULL)
-        g_file_monitor_emit_event (instance, event->child, event->other, event->event_type);
+      if (fms->instance)
+        g_file_monitor_emit_event (fms->instance, event->child, event->other, event->event_type);
 
-      g_clear_object (&instance);
       queued_event_free (event);
     }
 
@@ -576,28 +568,31 @@ g_file_monitor_source_dispatch (GSource     *source,
 static void
 g_file_monitor_source_dispose (GFileMonitorSource *fms)
 {
-  GHashTableIter iter;
-  gpointer seqiter;
-  QueuedEvent *event;
-
   g_mutex_lock (&fms->lock);
 
-  g_hash_table_iter_init (&iter, fms->pending_changes_table);
-  while (g_hash_table_iter_next (&iter, NULL, &seqiter))
+  if (fms->instance)
     {
-      g_hash_table_iter_remove (&iter);
-      g_sequence_remove (seqiter);
+      GHashTableIter iter;
+      gpointer seqiter;
+      QueuedEvent *event;
+
+      g_hash_table_iter_init (&iter, fms->pending_changes_table);
+      while (g_hash_table_iter_next (&iter, NULL, &seqiter))
+        {
+          g_hash_table_iter_remove (&iter);
+          g_sequence_remove (seqiter);
+        }
+
+      while ((event = g_queue_pop_head (&fms->event_queue)))
+        queued_event_free (event);
+
+      g_assert (g_sequence_is_empty (fms->pending_changes));
+      g_assert (g_hash_table_size (fms->pending_changes_table) == 0);
+      g_assert (fms->event_queue.length == 0);
+      fms->instance = NULL;
+
+      g_file_monitor_source_update_ready_time (fms);
     }
-
-  while ((event = g_queue_pop_head (&fms->event_queue)))
-    queued_event_free (event);
-
-  g_assert (g_sequence_is_empty (fms->pending_changes));
-  g_assert (g_hash_table_size (fms->pending_changes_table) == 0);
-  g_assert (fms->event_queue.length == 0);
-  g_weak_ref_set (&fms->instance_ref, NULL);
-
-  g_file_monitor_source_update_ready_time (fms);
 
   g_mutex_unlock (&fms->lock);
 
@@ -610,9 +605,7 @@ g_file_monitor_source_finalize (GSource *source)
   GFileMonitorSource *fms = (GFileMonitorSource *) source;
 
   /* should already have been cleared in dispose of the monitor */
-  g_assert (g_weak_ref_get (&fms->instance_ref) == NULL);
-  g_weak_ref_clear (&fms->instance_ref);
-
+  g_assert (fms->instance == NULL);
   g_assert (g_sequence_is_empty (fms->pending_changes));
   g_assert (g_hash_table_size (fms->pending_changes_table) == 0);
   g_assert (fms->event_queue.length == 0);
@@ -660,7 +653,7 @@ g_file_monitor_source_new (gpointer           instance,
   g_source_set_name (source, "GFileMonitorSource");
 
   g_mutex_init (&fms->lock);
-  g_weak_ref_init (&fms->instance_ref, instance);
+  fms->instance = instance;
   fms->pending_changes = g_sequence_new (pending_change_free);
   fms->pending_changes_table = g_hash_table_new (str_hash0, str_equal0);
   fms->rate_limit = DEFAULT_RATE_LIMIT;
@@ -894,7 +887,6 @@ g_local_file_monitor_new_in_worker (const gchar           *pathname,
                                     GFileMonitorFlags      flags,
                                     GFileMonitorCallback   callback,
                                     gpointer               user_data,
-                                    GClosureNotify         destroy_user_data,
                                     GError               **error)
 {
   GLocalFileMonitor *monitor;
@@ -907,8 +899,7 @@ g_local_file_monitor_new_in_worker (const gchar           *pathname,
   if (monitor)
     {
       if (callback)
-        g_signal_connect_data (monitor, "changed", G_CALLBACK (callback),
-                               user_data, destroy_user_data, 0  /* flags */);
+        g_signal_connect (monitor, "changed", G_CALLBACK (callback), user_data);
 
       g_local_file_monitor_start (monitor, pathname, is_directory, flags, GLIB_PRIVATE_CALL(g_get_worker_context) ());
     }
