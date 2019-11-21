efcc8bf922732268bb27ffe6190cd8893f28b009
openvela-robot <openvela-robot@xiaomi.com>
2019-11-21 10:57:27 +0800

    Merge branch '1940-atomic-types' into 'master'
    tests: Use pointers in calls to g_atomic_pointer_compare_and_exchange()
    
    Closes #1940
    
    See merge request GNOME/glib!1234

--- gio/tests/file.c
+++ gio/tests/file.c
@@ -929,8 +929,7 @@ test_copy_preserve_mode (void)
 
       dest_mode = g_file_info_get_attribute_uint32 (dest_info, G_FILE_ATTRIBUTE_UNIX_MODE);
 
-      g_assert_cmpint (dest_mode & ~S_IFMT, ==, vectors[i].expected_destination_mode);
-      g_assert_cmpint (dest_mode & S_IFMT, ==, S_IFREG);
+      g_assert_cmpint (dest_mode, ==, vectors[i].expected_destination_mode);
 
       (void) g_file_delete (tmpfile, NULL, NULL);
       (void) g_file_delete (dest_tmpfile, NULL, NULL);
