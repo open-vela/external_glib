83c3cc6ae192612174d4071003f2f7bf9bd150a0
liujinye <liujinye@xiaomi.com>
2020-01-29 12:54:32 +0800

    Merge branch 'BUG_running_docs' into 'master'
    docs: tag enclose 'all' and 'help' values
    
    See merge request GNOME/glib!1018

--- glib/gthread.h
+++ glib/gthread.h
@@ -254,7 +254,7 @@ void            g_once_init_leave               (volatile void  *location,
 # define g_once_init_leave(location, result) \
   (G_GNUC_EXTENSION ({                                               \
     G_STATIC_ASSERT (sizeof *(location) == sizeof (gpointer));       \
-    0 ? (void) (*(location) = (result)) : (void) 0;                  \
+    (void) (0 ? *(location) = (result) : 0);                         \
     g_once_init_leave ((location), (gsize) (result));                \
   }))
 #else
