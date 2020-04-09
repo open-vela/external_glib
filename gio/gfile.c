507948e5af15cab2b8558327769d1db1d4b9d510
openvela-robot <openvela-robot@xiaomi.com>
2020-04-09 11:51:46 +0800

    Merge branch 'override-dependency' into 'master'
    Meson: Override every dependency glib provides
    
    See merge request GNOME/glib!1441

--- gio/gfile.c
+++ gio/gfile.c
@@ -6839,8 +6839,7 @@ g_file_query_default_handler (GFile         *file,
     g_free (uri_scheme);
 
   info = g_file_query_info (file,
-                            G_FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE ","
-                            G_FILE_ATTRIBUTE_STANDARD_FAST_CONTENT_TYPE,
+                            G_FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE,
                             0,
                             cancellable,
                             error);
@@ -6850,8 +6849,6 @@ g_file_query_default_handler (GFile         *file,
   appinfo = NULL;
 
   content_type = g_file_info_get_content_type (info);
-  if (content_type == NULL)
-    content_type = g_file_info_get_attribute_string (info, G_FILE_ATTRIBUTE_STANDARD_FAST_CONTENT_TYPE);
   if (content_type)
     {
       /* Don't use is_native(), as we want to support fuse paths if available */
@@ -6893,8 +6890,6 @@ query_default_handler_query_info_cb (GObject      *object,
     }
 
   content_type = g_file_info_get_content_type (info);
-  if (content_type == NULL)
-    content_type = g_file_info_get_attribute_string (info, G_FILE_ATTRIBUTE_STANDARD_FAST_CONTENT_TYPE);
   if (content_type)
     {
       char *path;
@@ -6965,8 +6960,7 @@ g_file_query_default_handler_async (GFile              *file,
     g_free (uri_scheme);
 
   g_file_query_info_async (file,
-                           G_FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE ","
-                           G_FILE_ATTRIBUTE_STANDARD_FAST_CONTENT_TYPE,
+                           G_FILE_ATTRIBUTE_STANDARD_CONTENT_TYPE,
                            0,
                            io_priority,
                            cancellable,
