a38f5c63167dcc815d63c12ff0aafdc84930fdfb
openvela-robot <openvela-robot@xiaomi.com>
2020-05-07 08:41:23 +0800

    Merge branch 'th/g-ptr-array-extend-and-steal-crash' into 'master'
    array: fix corrupt state of GPtrArray after g_ptr_array_extend_and_steal()
    
    See merge request GNOME/glib!1481

--- glib/garray.c
+++ glib/garray.c
@@ -1186,6 +1186,7 @@ g_ptr_array_copy (GPtrArray *array,
                   GCopyFunc  func,
                   gpointer   user_data)
 {
+  gsize i;
   GPtrArray *new_array;
 
   g_return_val_if_fail (array != NULL, NULL);
@@ -1195,8 +1196,6 @@ g_ptr_array_copy (GPtrArray *array,
 
   if (func != NULL)
     {
-      guint i;
-
       for (i = 0; i < array->len; i++)
         new_array->pdata[i] = func (array->pdata[i], user_data);
     }
@@ -1459,8 +1458,7 @@ ptr_array_free (GPtrArray      *array,
       gpointer *stolen_pdata = g_steal_pointer (&rarray->pdata);
       if (rarray->element_free_func != NULL)
         {
-          guint i;
-
+          gsize i;
           for (i = 0; i < rarray->len; ++i)
             rarray->element_free_func (stolen_pdata[i]);
         }
@@ -1685,7 +1683,7 @@ g_ptr_array_remove_range (GPtrArray *array,
                           guint      length)
 {
   GRealPtrArray *rarray = (GRealPtrArray *)array;
-  guint i;
+  guint n;
 
   g_return_val_if_fail (rarray != NULL, NULL);
   g_return_val_if_fail (rarray->len == 0 || (rarray->len != 0 && rarray->pdata != NULL), NULL);
@@ -1694,8 +1692,8 @@ g_ptr_array_remove_range (GPtrArray *array,
 
   if (rarray->element_free_func != NULL)
     {
-      for (i = index_; i < index_ + length; i++)
-        rarray->element_free_func (rarray->pdata[i]);
+      for (n = index_; n < index_ + length; n++)
+        rarray->element_free_func (rarray->pdata[n]);
     }
 
   if (index_ + length != rarray->len)
@@ -1708,6 +1706,7 @@ g_ptr_array_remove_range (GPtrArray *array,
   rarray->len -= length;
   if (G_UNLIKELY (g_mem_gc_friendly))
     {
+      guint i;
       for (i = 0; i < length; i++)
         rarray->pdata[rarray->len + i] = NULL;
     }
@@ -1841,6 +1840,7 @@ g_ptr_array_extend (GPtrArray  *array_to_extend,
                     gpointer    user_data)
 {
   GRealPtrArray *rarray_to_extend = (GRealPtrArray *) array_to_extend;
+  gsize i;
 
   g_return_if_fail (array_to_extend != NULL);
   g_return_if_fail (array != NULL);
@@ -1849,8 +1849,6 @@ g_ptr_array_extend (GPtrArray  *array_to_extend,
 
   if (func != NULL)
     {
-      guint i;
-
       for (i = 0; i < array->len; i++)
         rarray_to_extend->pdata[i + rarray_to_extend->len] =
           func (array->pdata[i], user_data);
