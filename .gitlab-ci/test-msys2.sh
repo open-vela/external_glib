519c7cc540ce3153fed1bfe0e3589ef3caa6afc2
liujinye <liujinye@xiaomi.com>
2020-05-07 08:41:23 +0800

    Merge branch 'th/g-ptr-array-extend-and-steal-crash' into 'master'
    array: fix corrupt state of GPtrArray after g_ptr_array_extend_and_steal()
    
    See merge request GNOME/glib!1481

--- .gitlab-ci/test-msys2.sh
+++ .gitlab-ci/test-msys2.sh
@@ -38,7 +38,7 @@ export CCACHE_BASEDIR CCACHE_DIR
 
 pip3 install --upgrade --user meson==0.49.2
 
-PATH="$(cygpath "$USERPROFILE")/.local/bin:$HOME/.local/bin:$PATH"
+PATH="$HOME/.local/bin:$PATH"
 CFLAGS="-coverage -ftest-coverage -fprofile-arcs"
 DIR="$(pwd)"
 export PATH CFLAGS
