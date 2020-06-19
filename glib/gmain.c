c91e61324bc707c757f9206d54143a0109c43298
openvela-robot <openvela-robot@xiaomi.com>
2020-06-19 10:08:03 +0800

    Merge branch 'wip/oholy/remote-attribute-fixes' into 'master'
    Various GLocalFile fixes related to the filesystem::remote attribute
    
    See merge request GNOME/glib!1534

--- glib/gmain.c
+++ glib/gmain.c
@@ -448,10 +448,6 @@ static GMainContext *glib_worker_context;
 
 /* UNIX signals work by marking one of these variables then waking the
  * worker context to check on them and dispatch accordingly.
- *
- * Both variables must be accessed using atomic primitives, unless those atomic
- * primitives are implemented using fallback mutexes (as those aren’t safe in
- * an interrupt context).
  */
 #ifdef HAVE_SIG_ATOMIC_T
 static volatile sig_atomic_t unix_signal_pending[NSIG];
@@ -5203,7 +5199,7 @@ dispatch_unix_signals_unlocked (void)
   gint i;
 
   /* clear this first in case another one arrives while we're processing */
-  g_atomic_int_set (&any_unix_signal_pending, 0);
+  any_unix_signal_pending = FALSE;
 
   /* We atomically test/clear the bit from the global array in case
    * other signals arrive while we are dispatching.
@@ -5222,7 +5218,9 @@ dispatch_unix_signals_unlocked (void)
        *
        * Note specifically: we must check _our_ array.
        */
-      pending[i] = g_atomic_int_compare_and_exchange (&unix_signal_pending[i], 1, 0);
+      pending[i] = unix_signal_pending[i];
+      if (pending[i])
+        unix_signal_pending[i] = FALSE;
     }
 
   /* handle GChildWatchSource instances */
@@ -5528,14 +5526,8 @@ g_unix_signal_handler (int signum)
 {
   gint saved_errno = errno;
 
-#if defined(G_ATOMIC_LOCK_FREE) && defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4)
-  g_atomic_int_set (&unix_signal_pending[signum], 1);
-  g_atomic_int_set (&any_unix_signal_pending, 1);
-#else
-#warning "Can't use atomics in g_unix_signal_handler(): Unix signal handling will be racy"
-  unix_signal_pending[signum] = 1;
-  any_unix_signal_pending = 1;
-#endif
+  unix_signal_pending[signum] = TRUE;
+  any_unix_signal_pending = TRUE;
 
   g_wakeup_signal (glib_worker_context->wakeup);
 
@@ -6004,7 +5996,7 @@ glib_worker_main (gpointer data)
       g_main_context_iteration (glib_worker_context, TRUE);
 
 #ifdef G_OS_UNIX
-      if (g_atomic_int_get (&any_unix_signal_pending))
+      if (any_unix_signal_pending)
         dispatch_unix_signals ();
 #endif
     }
