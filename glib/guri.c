39a7cb7cfc72e3ed80b28eb3aadcc7d05a5f3643
liujinye <liujinye@xiaomi.com>
2020-11-17 14:23:17 +0800

    Merge branch '2046-add-shellcheck-pylint-in-ci' into 'master'
    resolve "Add pylint and shellcheck CI checks"
    
    Closes #2046
    
    See merge request GNOME/glib!1743

--- glib/guri.c
+++ glib/guri.c
@@ -752,7 +752,7 @@ static gboolean
 should_normalize_empty_path (const char *scheme)
 {
   const char * const schemes[] = { "https", "http", "wss", "ws" };
-  gsize i;
+  int i;
   for (i = 0; i < G_N_ELEMENTS (schemes); ++i)
     {
       if (!strcmp (schemes[i], scheme))
--- glib/tests/spawn-singlethread.c
+++ glib/tests/spawn-singlethread.c
@@ -179,7 +179,7 @@ test_spawn_async_with_fds (void)
   int tnum = 1;
   GPtrArray *argv;
   char *arg;
-  gsize i;
+  int i;
 
   /* Each test has 3 variable parameters: stdin, stdout, stderr */
   enum fd_type {
