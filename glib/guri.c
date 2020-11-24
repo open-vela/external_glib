dae99526ea953c52430f8c7651bb5754f18d4481
openvela-robot <openvela-robot@xiaomi.com>
2020-11-24 14:45:11 +0800

    Merge branch 'mcatanzaro/#2076' into 'master'
    Suppress some valgrind leak warnings
    
    Closes #2076
    
    See merge request GNOME/glib!1768

--- glib/guri.c
+++ glib/guri.c
@@ -794,21 +794,6 @@ normalize_port (const char *scheme,
   return port;
 }
 
-static int
-default_scheme_port (const char *scheme)
-{
-  if (strcmp (scheme, "http") == 0 || strcmp (scheme, "ws") == 0)
-    return 80;
-
-  if (strcmp (scheme, "https") == 0 || strcmp (scheme, "wss") == 0)
-    return 443;
-
-  if (strcmp (scheme, "ftp") == 0)
-    return 21;
-
-  return -1;
-}
-
 static gboolean
 g_uri_split_internal (const gchar  *uri_string,
                       GUriFlags     flags,
@@ -1004,8 +989,8 @@ g_uri_split_internal (const gchar  *uri_string,
           *path = g_strdup ("/");
         }
 
-      if (port && *port == -1)
-        *port = default_scheme_port (scheme_str);
+      if (port && *port != -1)
+        *port = normalize_port (scheme_str, *port);
     }
 
   g_free (normalized_scheme);
@@ -1583,7 +1568,6 @@ g_uri_join_internal (GUriFlags    flags,
 {
   gboolean encoded = (flags & G_URI_FLAGS_ENCODED);
   GString *str;
-  char *normalized_scheme = NULL;
 
   /* Restrictions on path prefixes. See:
    * https://tools.ietf.org/html/rfc3986#section-3
@@ -1595,9 +1579,6 @@ g_uri_join_internal (GUriFlags    flags,
   str = g_string_new (scheme);
   if (scheme)
     g_string_append_c (str, ':');
-
-  if (flags & G_URI_FLAGS_SCHEME_NORMALIZE && scheme && ((host && port != -1) || path[0] == '\0'))
-    normalized_scheme = g_ascii_strdown (scheme, -1);
 
   if (host)
     {
@@ -1659,18 +1640,14 @@ g_uri_join_internal (GUriFlags    flags,
             g_string_append_uri_escaped (str, host, HOST_ALLOWED_CHARS, TRUE);
         }
 
-      if (port != -1 && (!normalized_scheme || normalize_port (normalized_scheme, port) != -1))
+      if (port != -1)
         g_string_append_printf (str, ":%d", port);
     }
 
-  if (path[0] == '\0' && normalized_scheme && should_normalize_empty_path (normalized_scheme))
-    g_string_append (str, "/");
-  else if (encoded || flags & G_URI_FLAGS_ENCODED_PATH)
+  if (encoded || flags & G_URI_FLAGS_ENCODED_PATH)
     g_string_append (str, path);
   else
     g_string_append_uri_escaped (str, path, PATH_ALLOWED_CHARS, TRUE);
-
-  g_free (normalized_scheme);
 
   if (query)
     {
@@ -2454,9 +2431,6 @@ gint
 g_uri_get_port (GUri *uri)
 {
   g_return_val_if_fail (uri != NULL, -1);
-
-  if (uri->port == -1 && uri->flags & G_URI_FLAGS_SCHEME_NORMALIZE)
-    return default_scheme_port (uri->scheme);
 
   return uri->port;
 }
