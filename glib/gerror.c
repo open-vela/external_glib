c5220a51623dcedb4f341be0c8a86acb20c167e8
openvela-robot <openvela-robot@xiaomi.com>
2020-12-24 09:08:40 +0800

    Merge branch 'master' into 'master'
    Windows: fix FD_READ condition flag still set on recoverable UDP socket errors.
    
    See merge request GNOME/glib!1827

--- glib/gerror.c
+++ glib/gerror.c
@@ -382,20 +382,6 @@
 #include "gstrfuncs.h"
 #include "gtestutils.h"
 
-static GError *
-g_error_new_steal (GQuark  domain,
-                   gint    code,
-                   gchar  *message)
-{
-  GError *error = g_slice_new (GError);
-
-  error->domain = domain;
-  error->code = code;
-  error->message = message;
-
-  return error;
-}
-
 /**
  * g_error_new_valist:
  * @domain: error domain
@@ -416,6 +402,8 @@ g_error_new_valist (GQuark       domain,
                     const gchar *format,
                     va_list      args)
 {
+  GError *error;
+
   /* Historically, GError allowed this (although it was never meant to work),
    * and it has significant use in the wild, which g_return_val_if_fail
    * would break. It should maybe g_return_val_if_fail in GLib 4.
@@ -424,7 +412,13 @@ g_error_new_valist (GQuark       domain,
   g_warn_if_fail (domain != 0);
   g_warn_if_fail (format != NULL);
 
-  return g_error_new_steal (domain, code, g_strdup_vprintf (format, args));
+  error = g_slice_new (GError);
+
+  error->domain = domain;
+  error->code = code;
+  error->message = g_strdup_vprintf (format, args);
+
+  return error;
 }
 
 /**
@@ -476,10 +470,18 @@ g_error_new_literal (GQuark         domain,
                      gint           code,
                      const gchar   *message)
 {
+  GError* err;
+
   g_return_val_if_fail (message != NULL, NULL);
   g_return_val_if_fail (domain != 0, NULL);
 
-  return g_error_new_steal (domain, code, g_strdup (message));
+  err = g_slice_new (GError);
+
+  err->domain = domain;
+  err->code = code;
+  err->message = g_strdup (message);
+
+  return err;
 }
 
 /**
@@ -509,14 +511,20 @@ g_error_free (GError *error)
 GError*
 g_error_copy (const GError *error)
 {
+  GError *copy;
+ 
   g_return_val_if_fail (error != NULL, NULL);
   /* See g_error_new_valist for why these don't return */
   g_warn_if_fail (error->domain != 0);
   g_warn_if_fail (error->message != NULL);
 
-  return g_error_new_steal (error->domain,
-                            error->code,
-                            g_strdup (error->message));
+  copy = g_slice_new (GError);
+
+  *copy = *error;
+
+  copy->message = g_strdup (error->message);
+
+  return copy;
 }
 
 /**
