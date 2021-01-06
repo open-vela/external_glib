4e1f29fb0d74980610ccbf879d3657d46f446ef0
openvela-robot <openvela-robot@xiaomi.com>
2021-01-06 18:48:50 +0800

    Merge branch '1105-fix-codegen-unused' into 'master'
    Annotate function parameters with G_GNUC_UNUSED when necessary
    
    Closes #1105
    
    See merge request GNOME/glib!1858

--- gio/glocalfile.c
+++ gio/glocalfile.c
@@ -2038,7 +2038,7 @@ g_local_file_trash (GFile         *file,
     {
       uid_t uid;
       char uid_str[32];
-      gboolean success = FALSE;
+      gboolean success;
 
       uid = geteuid ();
       g_snprintf (uid_str, sizeof (uid_str), "%lu", (unsigned long)uid);
