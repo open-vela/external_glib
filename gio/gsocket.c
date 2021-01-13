fbe400e3ab8104fe4c9909cca468cf00250a3d12
openvela-robot <openvela-robot@xiaomi.com>
2021-01-13 13:56:42 +0800

    Merge branch 'error-tweaks' into 'master'
    GError documentation tweaks
    
    See merge request GNOME/glib!1880

--- gio/gsocket.c
+++ gio/gsocket.c
@@ -4784,37 +4784,18 @@ g_socket_send_message (GSocket                *socket,
   gsize bytes_written = 0;
   gsize vectors_size = 0;
 
-  if (num_vectors != -1)
+  for (gsize i = 0; i < num_vectors; i++)
     {
-      for (gint i = 0; i < num_vectors; i++)
+      /* No wrap-around for vectors_size */
+      if (vectors_size > vectors_size + vectors[i].size)
         {
-          /* No wrap-around for vectors_size */
-          if (vectors_size > vectors_size + vectors[i].size)
-            {
-              g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT,
-                           _("Unable to send message: %s"),
-                           _("Message vectors too large"));
-              return -1;
-            }
-
-          vectors_size += vectors[i].size;
+          g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT,
+                       _("Unable to send message: %s"),
+                       _("Message vectors too large"));
+          return -1;
         }
-    }
-  else
-    {
-      for (gsize i = 0; vectors[i].buffer != NULL; i++)
-        {
-          /* No wrap-around for vectors_size */
-          if (vectors_size > vectors_size + vectors[i].size)
-            {
-              g_set_error (error, G_IO_ERROR, G_IO_ERROR_INVALID_ARGUMENT,
-                           _("Unable to send message: %s"),
-                           _("Message vectors too large"));
-              return -1;
-            }
 
-          vectors_size += vectors[i].size;
-        }
+      vectors_size += vectors[i].size;
     }
 
   /* Check if vector's buffers are too big for gssize */
