2a3e22f6cb936e3967bcfae8a59f6f5c2759b6cb
openvela-robot <openvela-robot@xiaomi.com>
2021-02-25 18:26:57 +0800

    Merge branch 'pollable-test-dlopen-leak' into 'master'
    tests: Fix leak of dlopened module in pollable test
    
    See merge request GNOME/glib!1936

--- glib/gmem.h
+++ glib/gmem.h
@@ -31,11 +31,6 @@
 
 #include <glib/gutils.h>
 
-#if defined(glib_typeof_2_68) && GLIB_VERSION_MIN_REQUIRED >= GLIB_VERSION_2_68
-/* for glib_typeof */
-#include <type_traits>
-#endif
-
 G_BEGIN_DECLS
 
 /**
