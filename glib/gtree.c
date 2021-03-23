2171ebb8d4c99565405e4b56223cf51d58cef24d
liujinye <liujinye@xiaomi.com>
2021-03-23 16:52:15 +0800

    Merge branch '2361-key-file-locale-caching' into 'master'
    gkeyfile: Fix crash when parsing translations on a second load
    
    Closes #2361
    
    See merge request GNOME/glib!2009

--- glib/gtree.c
+++ glib/gtree.c
@@ -338,16 +338,7 @@ g_tree_node_next (GTreeNode *node)
   return tmp;
 }
 
-/**
- * g_tree_remove_all:
- * @tree: a #GTree
- *
- * Removes all nodes from a #GTree and destroys their keys and values,
- * then resets the #GTree’s root to %NULL.
- *
- * Since: 2.70
- */
-void
+static void
 g_tree_remove_all (GTree *tree)
 {
   GTreeNode *node;
