6d51fb7e8b22a2f2a7ffd701ba334e70f802ad79
liujinye <liujinye@xiaomi.com>
2021-04-05 10:42:33 +0800

    Merge branch 'fix_more_warnings' into 'master'
    Fix more warnings
    
    See merge request GNOME/glib!1957

--- gio/gapplication.c
+++ gio/gapplication.c
@@ -2524,12 +2524,7 @@ g_application_run (GApplication  *application,
 
   context = g_main_context_default ();
   acquired_context = g_main_context_acquire (context);
-  if (!acquired_context)
-    {
-      g_critical ("g_application_run() cannot acquire the default main context because it is already acquired by another thread!");
-      g_strfreev (arguments);
-      return 1;
-    }
+  g_return_val_if_fail (acquired_context, 0);
 
   if (!G_APPLICATION_GET_CLASS (application)
         ->local_command_line (application, &arguments, &status))
