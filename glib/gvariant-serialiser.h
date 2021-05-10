08a0cc3d2841346dc06277dd0419ecccbea211e6
liujinye <liujinye@xiaomi.com>
2021-05-11 07:27:24 +0800

    Merge branch 'optimise_gatomicrefcount' into 'master'
    grefcount: Optimise g_atomic_ref_count_inc
    
    Closes #1583
    
    See merge request GNOME/glib!2086

--- glib/gvariant-serialiser.h
+++ glib/gvariant-serialiser.h
@@ -31,14 +31,14 @@ typedef struct
   gsize             depth;  /* same semantics as GVariant.depth */
 } GVariantSerialised;
 
-/* deserialization */
+/* deserialisation */
 GLIB_AVAILABLE_IN_ALL
 gsize                           g_variant_serialised_n_children         (GVariantSerialised        container);
 GLIB_AVAILABLE_IN_ALL
 GVariantSerialised              g_variant_serialised_get_child          (GVariantSerialised        container,
                                                                          gsize                     index);
 
-/* serialization */
+/* serialisation */
 typedef void                  (*GVariantSerialisedFiller)               (GVariantSerialised       *serialised,
                                                                          gpointer                  data);
 
