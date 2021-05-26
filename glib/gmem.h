ae622b89c33429155459e5e647ea00577ce130f0
openvela-robot <openvela-robot@xiaomi.com>
2021-05-26 12:41:19 +0800

    Merge branch 'strv_builder_add_many' into 'master'
    gstrvbuilder: add addv and add_many to the API
    
    See merge request GNOME/glib!2112

--- glib/gmem.h
+++ glib/gmem.h
@@ -30,7 +30,11 @@
 #endif
 
 #include <glib/gutils.h>
-#include <glib/glib-typeof.h>
+
+#if defined(glib_typeof_2_68) && GLIB_VERSION_MIN_REQUIRED >= GLIB_VERSION_2_68
+/* for glib_typeof */
+#include <type_traits>
+#endif
 
 G_BEGIN_DECLS
 
@@ -111,7 +115,7 @@ gpointer g_try_realloc_n  (gpointer	 mem,
 			   gsize	 n_blocks,
 			   gsize	 n_block_bytes) G_GNUC_WARN_UNUSED_RESULT;
 
-#if defined(glib_typeof) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_58
+#if defined(glib_typeof) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_58 && (!defined(glib_typeof_2_68) || GLIB_VERSION_MIN_REQUIRED >= GLIB_VERSION_2_68)
 #define g_clear_pointer(pp, destroy)                     \
   G_STMT_START                                           \
   {                                                      \
@@ -214,7 +218,7 @@ g_steal_pointer (gpointer pp)
 }
 
 /* type safety */
-#if defined(glib_typeof) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_58
+#if defined(glib_typeof) && GLIB_VERSION_MAX_ALLOWED >= GLIB_VERSION_2_58 && (!defined(glib_typeof_2_68) || GLIB_VERSION_MIN_REQUIRED >= GLIB_VERSION_2_68)
 #define g_steal_pointer(pp) ((glib_typeof (*pp)) (g_steal_pointer) (pp))
 #else  /* __GNUC__ */
 /* This version does not depend on gcc extensions, but gcc does not warn
