449bdc66a5724ce7251818870b18024ca5be3a01
openvela-robot <openvela-robot@xiaomi.com>
2021-06-07 13:15:52 +0800

    Merge branch '2417-file-replace-contents-etags' into 'master'
    glocalfileoutputstream: Fix ETag check when replacing through a symlink
    
    Closes #2417
    
    See merge request GNOME/glib!2138

--- .gitlab-ci/meson-junit-report.py
+++ .gitlab-ci/meson-junit-report.py
@@ -27,7 +27,7 @@ aparser.add_argument(
     "--branch",
     metavar="NAME",
     help="Branch of the project being tested",
-    default="main",
+    default="master",
 )
 aparser.add_argument(
     "--output",
--- .gitlab-ci/run-style-check-diff.sh
+++ .gitlab-ci/run-style-check-diff.sh
@@ -25,7 +25,7 @@ exit_status=$?
 # style with the changes they’re making.)
 echo ""
 echo "Note that clang-format output is advisory and cannot always match the GLib coding style, documented at"
-echo "   https://gitlab.gnome.org/GNOME/gtk/blob/HEAD/docs/CODING-STYLE.md"
+echo "   https://gitlab.gnome.org/GNOME/gtk/blob/master/docs/CODING-STYLE"
 echo "Warnings from this tool can be ignored in favour of the documented coding style,"
 echo "or in favour of matching the style of existing surrounding code."
 
