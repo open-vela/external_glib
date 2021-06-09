09214d48e482a5c7538cb4d0f47edc77de3a8073
openvela-robot <openvela-robot@xiaomi.com>
2021-06-09 12:49:04 +0800

    Merge branch 'confusing_errors' into 'main'
    gdbusconnection: removed a confusing message
    
    Closes #793
    
    See merge request GNOME/glib!2118

--- glib/tests/regex.c
+++ glib/tests/regex.c
@@ -2675,7 +2675,7 @@ main (int argc, char *argv[])
   TEST_EXPAND("a", "a", "\\0130", FALSE, "X");
   TEST_EXPAND("a", "a", "\\\\\\0", FALSE, "\\a");
   TEST_EXPAND("a(?P<G>.)c", "xabcy", "X\\g<G>X", FALSE, "XbX");
-#if !(PCRE_MAJOR > 8 || (PCRE_MAJOR == 8 && PCRE_MINOR >= 34))
+#ifndef USE_SYSTEM_PCRE
   /* PCRE >= 8.34 no longer allows this usage. */
   TEST_EXPAND("(.)(?P<1>.)", "ab", "\\1", FALSE, "a");
   TEST_EXPAND("(.)(?P<1>.)", "ab", "\\g<1>", FALSE, "a");
