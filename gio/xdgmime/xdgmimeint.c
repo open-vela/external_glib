d926d13a4b40bc3bd77543ea706259d824c80fa5
openvela-robot <openvela-robot@xiaomi.com>
2021-09-23 10:24:26 +0800

    Merge branch '2401-object-manager-warning' into 'main'
    gdbusobjectmanagerclient: Don’t warn if removing an interface fails
    
    Closes #2401
    
    See merge request GNOME/glib!2263

--- gio/xdgmime/xdgmimeint.c
+++ gio/xdgmime/xdgmimeint.c
@@ -12,7 +12,7 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,14 +20,10 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+#include "config.h"
 
 #include "xdgmimeint.h"
 #include <ctype.h>
@@ -193,7 +189,7 @@ const char *
 _xdg_binary_or_text_fallback(const void *data, size_t len)
 {
   unsigned char *chardata;
-  size_t i;
+  int i;
 
   chardata = (unsigned char *) data;
   for (i = 0; i < 128 && i < len; ++i)
