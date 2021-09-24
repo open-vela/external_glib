bcd88d5b6a9097665891945deeaf91dc90f4e790
openvela-robot <openvela-robot@xiaomi.com>
2021-09-24 14:26:06 +0800

    Merge branch 'fix_atomic_cast' into 'main'
    Coerce type cast to void* because it causes compiler warnings
    
    See merge request GNOME/glib!2114

--- gio/xdgmime/xdgmimeicon.c
+++ gio/xdgmime/xdgmimeicon.c
@@ -19,14 +19,10 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+#include "config.h"
 
 #include "xdgmimeicon.h"
 #include "xdgmimeint.h"
@@ -182,4 +178,3 @@ _xdg_mime_icon_list_dump (XdgIconList *list)
 }
 
 #endif
-
