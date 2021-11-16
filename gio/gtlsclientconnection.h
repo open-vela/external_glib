f2c8ab1a8dd5b4552eaa5402be84a7e5ca74e63f
liujinye <liujinye@xiaomi.com>
2021-11-16 15:35:05 +0800

    Merge branch 'main' into 'main'
    Better detection of the cleanup attribute.
    
    See merge request GNOME/glib!2223

--- gio/gtlsclientconnection.h
+++ gio/gtlsclientconnection.h
@@ -59,9 +59,9 @@ GIOStream *           g_tls_client_connection_new                  (GIOStream   
 								    GSocketConnectable      *server_identity,
 								    GError                 **error);
 
-GLIB_DEPRECATED_IN_2_72
+GLIB_AVAILABLE_IN_ALL
 GTlsCertificateFlags  g_tls_client_connection_get_validation_flags (GTlsClientConnection    *conn);
-GLIB_DEPRECATED_IN_2_72
+GLIB_AVAILABLE_IN_ALL
 void                  g_tls_client_connection_set_validation_flags (GTlsClientConnection    *conn,
 								    GTlsCertificateFlags     flags);
 GLIB_AVAILABLE_IN_ALL
