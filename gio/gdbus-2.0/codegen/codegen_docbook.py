95bc29367a8a667b927c24b79c2131a9bd9241d1
liujinye <liujinye@xiaomi.com>
2022-02-15 11:52:46 +0800

    Merge branch 'wip/smcv/aligned-alloc-tests' into 'main'
    Fix some problems with g_aligned_alloc() tests
    
    See merge request GNOME/glib!2488

--- gio/gdbus-2.0/codegen/codegen_docbook.py
+++ gio/gdbus-2.0/codegen/codegen_docbook.py
@@ -25,6 +25,10 @@ from os import path
 from . import utils
 
 
+# Disable line length warnings as wrapping the Docbook templates would be hard
+# flake8: noqa: E501
+
+
 # ----------------------------------------------------------------------------------------------------
 
 
@@ -342,7 +346,7 @@ class DocbookCodeGenerator:
     def expand_paras(self, s, expandParamsAndConstants):
         s = self.expand(s, expandParamsAndConstants).strip()
         res = []
-        if not s.startswith("<para>"):
+        if not s.startswith("<para"):
             res.append("<para>")
         for line in s.split("\n"):
             line = line.strip()
