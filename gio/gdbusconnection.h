85a04a86dfa97d81db31646c9a4ebce250e533ab
liujinye <liujinye@xiaomi.com>
2022-04-05 11:19:51 +0800

    Merge branch 'cleanup-warnings-split-9' into 'main'
    Cleanup warnings split 9
    
    See merge request GNOME/glib!2498

--- gio/gdbusconnection.h
+++ gio/gdbusconnection.h
@@ -218,9 +218,6 @@ GVariant *g_dbus_connection_call_sync                         (GDBusConnection  
                                                                gint                timeout_msec,
                                                                GCancellable       *cancellable,
                                                                GError            **error);
-
-#ifdef G_OS_UNIX
-
 GLIB_AVAILABLE_IN_2_30
 void      g_dbus_connection_call_with_unix_fd_list            (GDBusConnection    *connection,
                                                                const gchar        *bus_name,
@@ -254,8 +251,6 @@ GVariant *g_dbus_connection_call_with_unix_fd_list_sync       (GDBusConnection  
                                                                GUnixFDList       **out_fd_list,
                                                                GCancellable       *cancellable,
                                                                GError            **error);
-
-#endif /* G_OS_UNIX */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
