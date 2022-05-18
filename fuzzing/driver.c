220ae8b869cf0d075d251ec3b2580892a9e53163
liujinye <liujinye@xiaomi.com>
2022-05-18 09:09:24 +0800

    Merge branch 'reuse-more' into 'main'
    glib/gio/tests: Add SPDX license headers automatically
    
    See merge request GNOME/glib!2665

--- fuzzing/driver.c
+++ fuzzing/driver.c
@@ -1,12 +1,3 @@
-/*
- * Copyright 2018 LLVM contributors
- *
- * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
- *
- * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
- * See https://llvm.org/LICENSE.txt for license information.
- */
-
 /* Simpler gnu89 version of StandaloneFuzzTargetMain.c from LLVM */
 
 #include <assert.h>
