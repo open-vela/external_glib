f7757ba9c50efe87745094e86f786b5523e7489d
openvela-robot <openvela-robot@xiaomi.com>
2022-05-25 13:41:12 +0800

    Merge branch 'param-speedups2' into 'main'
    param: Add a value_is_valid vfunc
    
    See merge request GNOME/glib!2677

--- .gitlab-ci/run-tests.sh
+++ .gitlab-ci/run-tests.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+set +e
+
+case "$1" in
+  --log-file)
+    log_file="$2"
+    shift
+    shift
+    ;;
+  *)
+    log_file="_build/meson-logs/testlog.json"
+esac
+
 meson test \
         -C _build \
         --timeout-multiplier "${MESON_TEST_TIMEOUT_MULTIPLIER}" \
         --no-suite flaky \
         "$@"
+
+exit_code=$?
+
+exit $exit_code
