57ab0aa5c364d8bf36061ef9bb271ffe278c606f
openvela-robot <openvela-robot@xiaomi.com>
2022-05-25 12:22:03 +0800

    Merge branch 'junit' into 'main'
    gitlab-ci: remove meson-junit-report.py conversion
    
    See merge request GNOME/glib!2686

--- gobject/gparamspecs.c
+++ gobject/gparamspecs.c
@@ -81,16 +81,6 @@ param_char_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_char_is_valid (GParamSpec   *pspec,
-                     const GValue *value)
-{
-  GParamSpecChar *cspec = G_PARAM_SPEC_CHAR (pspec);
-  gint oval = value->data[0].v_int;
-  
-  return cspec->minimum <= oval && oval <= cspec->maximum;
-}
-
-static gboolean
 param_char_validate (GParamSpec *pspec,
 		     GValue     *value)
 {
@@ -120,16 +110,6 @@ param_uchar_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_uchar_is_valid (GParamSpec   *pspec,
-		      const GValue *value)
-{
-  GParamSpecUChar *uspec = G_PARAM_SPEC_UCHAR (pspec);
-  guint oval = value->data[0].v_uint;
-  
-  return uspec->minimum <= oval && oval <= uspec->maximum;
-}
-
-static gboolean
 param_uchar_validate (GParamSpec *pspec,
 		      GValue     *value)
 {
@@ -146,15 +126,6 @@ param_boolean_set_default (GParamSpec *pspec,
 			   GValue     *value)
 {
   value->data[0].v_int = G_PARAM_SPEC_BOOLEAN (pspec)->default_value;
-}
-
-static gboolean
-param_boolean_is_valid (GParamSpec   *pspec,
-                        const GValue *value)
-{
-  int oval = value->data[0].v_int;
-
-  return oval == FALSE || oval == TRUE;
 }
 
 static gboolean
@@ -183,16 +154,6 @@ param_int_set_default (GParamSpec *pspec,
 		       GValue     *value)
 {
   value->data[0].v_int = G_PARAM_SPEC_INT (pspec)->default_value;
-}
-
-static gboolean
-param_int_is_valid (GParamSpec   *pspec,
-                    const GValue *value)
-{
-  GParamSpecInt *ispec = G_PARAM_SPEC_INT (pspec);
-  int oval = value->data[0].v_int;
-
-  return ispec->minimum <= oval && oval <= ispec->maximum;
 }
 
 static gboolean
@@ -233,16 +194,6 @@ param_uint_set_default (GParamSpec *pspec,
 			GValue     *value)
 {
   value->data[0].v_uint = G_PARAM_SPEC_UINT (pspec)->default_value;
-}
-
-static gboolean
-param_uint_is_valid (GParamSpec   *pspec,
-		     const GValue *value)
-{
-  GParamSpecUInt *uspec = G_PARAM_SPEC_UINT (pspec);
-  guint oval = value->data[0].v_uint;
-  
-  return uspec->minimum <= oval && oval <= uspec->maximum;
 }
 
 static gboolean
@@ -291,16 +242,6 @@ param_long_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_long_is_valid (GParamSpec   *pspec,
-		     const GValue *value)
-{
-  GParamSpecLong *lspec = G_PARAM_SPEC_LONG (pspec);
-  glong oval = value->data[0].v_long;
-  
-  return lspec->minimum <= oval && oval <= lspec->maximum;
-}
-
-static gboolean
 param_long_validate (GParamSpec *pspec,
 		     GValue     *value)
 {
@@ -345,16 +286,6 @@ param_ulong_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_ulong_is_valid (GParamSpec   *pspec,
-                      const GValue *value)
-{
-  GParamSpecULong *uspec = G_PARAM_SPEC_ULONG (pspec);
-  gulong oval = value->data[0].v_ulong;
-  
-  return uspec->minimum <= oval && oval <= uspec->maximum;
-}
-
-static gboolean
 param_ulong_validate (GParamSpec *pspec,
 		      GValue     *value)
 {
@@ -392,16 +323,6 @@ param_int64_set_default (GParamSpec *pspec,
 			GValue     *value)
 {
   value->data[0].v_int64 = G_PARAM_SPEC_INT64 (pspec)->default_value;
-}
-
-static gboolean
-param_int64_is_valid (GParamSpec   *pspec,
-                      const GValue *value)
-{
-  GParamSpecInt64 *lspec = G_PARAM_SPEC_INT64 (pspec);
-  gint64 oval = value->data[0].v_int64;
-  
-  return lspec->minimum <= oval && oval <= lspec->maximum;
 }
 
 static gboolean
@@ -445,16 +366,6 @@ param_uint64_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_uint64_is_valid (GParamSpec   *pspec,
-                       const GValue *value)
-{
-  GParamSpecUInt64 *uspec = G_PARAM_SPEC_UINT64 (pspec);
-  guint64 oval = value->data[0].v_uint64;
-  
-  return uspec->minimum <= oval && oval <= uspec->maximum;
-}
-
-static gboolean
 param_uint64_validate (GParamSpec *pspec,
 		      GValue     *value)
 {
@@ -490,13 +401,6 @@ param_unichar_set_default (GParamSpec *pspec,
 			 GValue     *value)
 {
   value->data[0].v_uint = G_PARAM_SPEC_UNICHAR (pspec)->default_value;
-}
-
-static gboolean
-param_unichar_is_valid (GParamSpec   *pspec,
-                        const GValue *value)
-{
-  return g_unichar_validate (value->data[0].v_uint);
 }
 
 static gboolean
@@ -558,16 +462,6 @@ param_enum_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_enum_is_valid (GParamSpec   *pspec,
-                     const GValue *value)
-{
-  GParamSpecEnum *espec = G_PARAM_SPEC_ENUM (pspec);
-  glong oval = value->data[0].v_long;
-  
-  return g_enum_get_value (espec->enum_class, oval) != NULL;
-}
-
-static gboolean
 param_enum_validate (GParamSpec *pspec,
 		     GValue     *value)
 {
@@ -613,15 +507,6 @@ param_flags_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_flags_is_valid (GParamSpec   *pspec,
-                      const GValue *value)
-{
-  GParamSpecFlags *fspec = G_PARAM_SPEC_FLAGS (pspec);
-  gulong oval = value->data[0].v_ulong;
-  
-  return (oval & ~fspec->flags_class->mask) == 0;
-}
-static gboolean
 param_flags_validate (GParamSpec *pspec,
 		      GValue     *value)
 {
@@ -652,16 +537,6 @@ param_float_set_default (GParamSpec *pspec,
 			 GValue     *value)
 {
   value->data[0].v_float = G_PARAM_SPEC_FLOAT (pspec)->default_value;
-}
-
-static gboolean
-param_float_is_valid (GParamSpec   *pspec,
-                      const GValue *value)
-{
-  GParamSpecFloat *fspec = G_PARAM_SPEC_FLOAT (pspec);
-  gfloat oval = value->data[0].v_float;
-  
-  return fspec->minimum <= oval && oval <= fspec->maximum;
 }
 
 static gboolean
@@ -705,16 +580,6 @@ param_double_set_default (GParamSpec *pspec,
 			  GValue     *value)
 {
   value->data[0].v_double = G_PARAM_SPEC_DOUBLE (pspec)->default_value;
-}
-
-static gboolean
-param_double_is_valid (GParamSpec   *pspec,
-                       const GValue *value)
-{
-  GParamSpecDouble *dspec = G_PARAM_SPEC_DOUBLE (pspec);
-  gfloat oval = value->data[0].v_double;
-  
-  return dspec->minimum <= oval && oval <= dspec->maximum;
 }
 
 static gboolean
@@ -837,29 +702,6 @@ param_string_validate (GParamSpec *pspec,
   return changed;
 }
 
-static gboolean
-param_string_is_valid (GParamSpec   *pspec,
-                       const GValue *value)
-{
-  GParamSpecString *sspec = G_PARAM_SPEC_STRING (pspec);
-  gboolean ret = TRUE;
-
-  if (sspec->cset_first != NULL || sspec->cset_nth != NULL ||
-      sspec->ensure_non_null || sspec->null_fold_if_empty)
-    {
-      GValue tmp_value = G_VALUE_INIT;
-
-      g_value_init (&tmp_value, G_VALUE_TYPE (value));
-      g_value_copy (value, &tmp_value);
-
-      ret = !param_string_validate (pspec, &tmp_value);
-
-      g_value_unset (&tmp_value);
-    }
-
-  return ret;
-}
-
 static gint
 param_string_values_cmp (GParamSpec   *pspec,
 			 const GValue *value1,
@@ -884,15 +726,6 @@ param_param_set_default (GParamSpec *pspec,
 			 GValue     *value)
 {
   value->data[0].v_pointer = NULL;
-}
-
-static gboolean
-param_param_is_valid (GParamSpec   *pspec,
-                      const GValue *value)
-{
-  GParamSpec *param = value->data[0].v_pointer;
-
-  return g_value_type_compatible (G_PARAM_SPEC_TYPE (param), G_PARAM_SPEC_VALUE_TYPE (pspec));
 }
 
 static gboolean
@@ -950,6 +783,16 @@ param_pointer_set_default (GParamSpec *pspec,
 			   GValue     *value)
 {
   value->data[0].v_pointer = NULL;
+}
+
+static gboolean
+param_pointer_validate (GParamSpec *pspec,
+			GValue     *value)
+{
+  /* GParamSpecPointer *spec = G_PARAM_SPEC_POINTER (pspec); */
+  guint changed = 0;
+  
+  return changed;
 }
 
 static gint
@@ -1130,17 +973,6 @@ param_object_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_object_is_valid (GParamSpec   *pspec,
-                       const GValue *value)
-{
-  GParamSpecObject *ospec = G_PARAM_SPEC_OBJECT (pspec);
-  GObject *object = value->data[0].v_pointer;
-
-  return object &&
-         g_value_type_compatible (G_OBJECT_TYPE (object), G_PARAM_SPEC_VALUE_TYPE (ospec));
-}
-
-static gboolean
 param_object_validate (GParamSpec *pspec,
 		       GValue     *value)
 {
@@ -1202,15 +1034,6 @@ param_override_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_override_is_valid (GParamSpec   *pspec,
-			 const GValue *value)
-{
-  GParamSpecOverride *ospec = G_PARAM_SPEC_OVERRIDE (pspec);
-
-  return g_param_value_is_valid (ospec->overridden, value);
-}
-
-static gboolean
 param_override_validate (GParamSpec *pspec,
 			 GValue     *value)
 {
@@ -1241,17 +1064,6 @@ param_gtype_set_default (GParamSpec *pspec,
   GParamSpecGType *tspec = G_PARAM_SPEC_GTYPE (pspec);
 
   value->data[0].v_pointer = GSIZE_TO_POINTER (tspec->is_a_type);
-}
-
-static gboolean
-param_gtype_is_valid (GParamSpec   *pspec,
-                      const GValue *value)
-{
-  GParamSpecGType *tspec = G_PARAM_SPEC_GTYPE (pspec);
-  GType gtype = GPOINTER_TO_SIZE (value->data[0].v_pointer);
-  
-  return tspec->is_a_type == G_TYPE_NONE ||
-         g_type_is_a (gtype, tspec->is_a_type);
 }
 
 static gboolean
@@ -1315,19 +1127,6 @@ param_variant_set_default (GParamSpec *pspec,
 }
 
 static gboolean
-param_variant_is_valid (GParamSpec   *pspec,
-                        const GValue *value)
-{
-  GParamSpecVariant *vspec = G_PARAM_SPEC_VARIANT (pspec);
-  GVariant *variant = value->data[0].v_pointer;
-
-  if (variant == NULL)
-    return vspec->default_value == NULL;
-  else
-    return g_variant_is_of_type (variant, vspec->type);
-}
-
-static gboolean
 param_variant_validate (GParamSpec *pspec,
                         GValue     *value)
 {
@@ -1382,13 +1181,6 @@ param_variant_values_cmp (GParamSpec   *pspec,
 }
 
 /* --- type initialization --- */
-
-#define set_is_valid_vfunc(type,func) { \
-  GParamSpecClass *class = g_type_class_ref (type); \
-  class->value_is_valid = func; \
-  g_type_class_unref (class); \
-}
-
 GType *g_param_spec_types = NULL;
 
 void
@@ -1420,7 +1212,6 @@ _g_param_spec_types_init (void)
       param_int_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamChar"), &pspec_info);
-    set_is_valid_vfunc (type, param_char_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_CHAR);
   }
@@ -1439,7 +1230,6 @@ _g_param_spec_types_init (void)
       param_uint_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamUChar"), &pspec_info);
-    set_is_valid_vfunc (type, param_uchar_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_UCHAR);
   }
@@ -1458,7 +1248,6 @@ _g_param_spec_types_init (void)
       param_int_values_cmp,       /* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamBoolean"), &pspec_info);
-    set_is_valid_vfunc (type, param_boolean_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_BOOLEAN);
   }
@@ -1477,7 +1266,6 @@ _g_param_spec_types_init (void)
       param_int_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamInt"), &pspec_info);
-    set_is_valid_vfunc (type, param_int_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_INT);
   }
@@ -1496,7 +1284,6 @@ _g_param_spec_types_init (void)
       param_uint_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamUInt"), &pspec_info);
-    set_is_valid_vfunc (type, param_uint_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_UINT);
   }
@@ -1515,7 +1302,6 @@ _g_param_spec_types_init (void)
       param_long_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamLong"), &pspec_info);
-    set_is_valid_vfunc (type, param_long_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_LONG);
   }
@@ -1534,7 +1320,6 @@ _g_param_spec_types_init (void)
       param_ulong_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamULong"), &pspec_info);
-    set_is_valid_vfunc (type, param_ulong_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_ULONG);
   }
@@ -1553,7 +1338,6 @@ _g_param_spec_types_init (void)
       param_int64_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamInt64"), &pspec_info);
-    set_is_valid_vfunc (type, param_int64_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_INT64);
   }
@@ -1572,7 +1356,6 @@ _g_param_spec_types_init (void)
       param_uint64_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamUInt64"), &pspec_info);
-    set_is_valid_vfunc (type, param_uint64_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_UINT64);
   }
@@ -1591,7 +1374,6 @@ _g_param_spec_types_init (void)
       param_unichar_values_cmp,	 /* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamUnichar"), &pspec_info);
-    set_is_valid_vfunc (type, param_unichar_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_UNICHAR);
   }
@@ -1610,7 +1392,6 @@ _g_param_spec_types_init (void)
       param_long_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamEnum"), &pspec_info);
-    set_is_valid_vfunc (type, param_enum_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_ENUM);
   }
@@ -1629,7 +1410,6 @@ _g_param_spec_types_init (void)
       param_ulong_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamFlags"), &pspec_info);
-    set_is_valid_vfunc (type, param_flags_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_FLAGS);
   }
@@ -1648,7 +1428,6 @@ _g_param_spec_types_init (void)
       param_float_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamFloat"), &pspec_info);
-    set_is_valid_vfunc (type, param_float_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_FLOAT);
   }
@@ -1667,7 +1446,6 @@ _g_param_spec_types_init (void)
       param_double_values_cmp,		/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamDouble"), &pspec_info);
-    set_is_valid_vfunc (type, param_double_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_DOUBLE);
   }
@@ -1686,7 +1464,6 @@ _g_param_spec_types_init (void)
       param_string_values_cmp,		/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamString"), &pspec_info);
-    set_is_valid_vfunc (type, param_string_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_STRING);
   }
@@ -1705,7 +1482,6 @@ _g_param_spec_types_init (void)
       param_pointer_values_cmp,	/* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamParam"), &pspec_info);
-    set_is_valid_vfunc (type, param_param_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_PARAM);
   }
@@ -1738,7 +1514,7 @@ _g_param_spec_types_init (void)
       G_TYPE_POINTER,  		   /* value_type */
       NULL,			   /* finalize */
       param_pointer_set_default,   /* value_set_default */
-      NULL,
+      param_pointer_validate,	   /* value_validate */
       param_pointer_values_cmp,	   /* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamPointer"), &pspec_info);
@@ -1779,7 +1555,6 @@ _g_param_spec_types_init (void)
       param_object_values_cmp,	 /* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamObject"), &pspec_info);
-    set_is_valid_vfunc (type, param_object_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_OBJECT);
   }
@@ -1798,7 +1573,6 @@ _g_param_spec_types_init (void)
       param_override_values_cmp,  /* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamOverride"), &pspec_info);
-    set_is_valid_vfunc (type, param_override_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_OVERRIDE);
   }
@@ -1818,7 +1592,6 @@ _g_param_spec_types_init (void)
     };
     pspec_info.value_type = G_TYPE_GTYPE;
     type = g_param_type_register_static (g_intern_static_string ("GParamGType"), &pspec_info);
-    set_is_valid_vfunc (type, param_gtype_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_GTYPE);
   }
@@ -1837,7 +1610,6 @@ _g_param_spec_types_init (void)
       param_variant_values_cmp,   /* values_cmp */
     };
     type = g_param_type_register_static (g_intern_static_string ("GParamVariant"), &pspec_info);
-    set_is_valid_vfunc (type, param_variant_is_valid);
     *spec_types++ = type;
     g_assert (type == G_TYPE_PARAM_VARIANT);
   }
--- gobject/tests/param.c
+++ gobject/tests/param.c
@@ -48,42 +48,34 @@ test_param_spec_char (void)
   g_assert_true (g_param_value_defaults (pspec, &value));
 
   g_value_set_char (&value, 0);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 20);
 
   g_value_set_char (&value, 20);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 20);
 
   g_value_set_char (&value, 40);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 40);
 
   g_value_set_char (&value, 60);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 40);
 
   g_value_set_schar (&value, 0);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_schar (&value), ==, 20);
 
   g_value_set_schar (&value, 20);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_schar (&value), ==, 20);
 
   g_value_set_schar (&value, 40);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_schar (&value), ==, 40);
 
   g_value_set_schar (&value, 60);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_schar (&value), ==, 40);
 
@@ -101,11 +93,9 @@ test_param_spec_string (void)
   g_value_init (&value, G_TYPE_STRING);
 
   g_value_set_string (&value, "foobar");
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
 
   g_value_set_string (&value, "");
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_nonnull (g_value_get_string (&value));
 
@@ -114,7 +104,6 @@ test_param_spec_string (void)
   G_PARAM_SPEC_STRING (pspec)->ensure_non_null = TRUE;
 
   g_value_set_string (&value, NULL);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_nonnull (g_value_get_string (&value));
 
@@ -125,12 +114,10 @@ test_param_spec_string (void)
   G_PARAM_SPEC_STRING (pspec)->null_fold_if_empty = TRUE;
 
   g_value_set_string (&value, "");
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_null (g_value_get_string (&value));
 
   g_value_set_static_string (&value, "");
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_null (g_value_get_string (&value));
 
@@ -142,12 +129,10 @@ test_param_spec_string (void)
   G_PARAM_SPEC_STRING (pspec)->substitutor = '-';
 
   g_value_set_string (&value, "ABC");
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_string (&value)[0], ==, '-');
 
   g_value_set_static_string (&value, "ABC");
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_string (&value)[0], ==, '-');
 
@@ -156,12 +141,10 @@ test_param_spec_string (void)
   G_PARAM_SPEC_STRING (pspec)->cset_nth = g_strdup ("abc");
 
   g_value_set_string (&value, "aBC");
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_string (&value)[1], ==, '-');
 
   g_value_set_static_string (&value, "aBC");
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_string (&value)[1], ==, '-');
 
@@ -190,22 +173,18 @@ test_param_spec_override (void)
   g_assert_true (g_param_value_defaults (pspec, &value));
 
   g_value_set_char (&value, 0);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 20);
 
   g_value_set_char (&value, 20);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 20);
 
   g_value_set_char (&value, 40);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 40);
 
   g_value_set_char (&value, 60);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_char (&value), ==, 40);
 
@@ -228,12 +207,10 @@ test_param_spec_gtype (void)
   g_assert_true (g_param_value_defaults (pspec, &value));
 
   g_value_set_gtype (&value, G_TYPE_INT);
-  g_assert_false (g_param_value_is_valid (pspec, &value));
   g_assert_true (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_gtype (&value), ==, G_TYPE_PARAM);
 
   g_value_set_gtype (&value, G_TYPE_PARAM_INT);
-  g_assert_true (g_param_value_is_valid (pspec, &value));
   g_assert_false (g_param_value_validate (pspec, &value));
   g_assert_cmpint (g_value_get_gtype (&value), ==, G_TYPE_PARAM_INT);
 
@@ -249,6 +226,7 @@ test_param_spec_variant (void)
   GValue value3 = G_VALUE_INIT;
   GValue value4 = G_VALUE_INIT;
   GValue value5 = G_VALUE_INIT;
+  gboolean modified;
 
   pspec = g_param_spec_variant ("variant", "nick", "blurb",
                                 G_VARIANT_TYPE ("i"),
@@ -276,14 +254,13 @@ test_param_spec_variant (void)
   g_assert_false (g_param_value_defaults (pspec, &value4));
   g_assert_false (g_param_value_defaults (pspec, &value5));
 
-  g_assert_true (g_param_value_is_valid (pspec, &value));
-  g_assert_false (g_param_value_validate (pspec, &value));
+  modified = g_param_value_validate (pspec, &value);
+  g_assert_false (modified);
 
   g_value_reset (&value);
   g_value_set_variant (&value, g_variant_new_uint32 (41));
-  g_assert_false (g_param_value_is_valid (pspec, &value));
-  g_assert_true (g_param_value_validate (pspec, &value));
-  g_assert_true (g_param_value_is_valid (pspec, &value));
+  modified = g_param_value_validate (pspec, &value);
+  g_assert_true (modified);
   g_assert_cmpint (g_variant_get_int32 (g_value_get_variant (&value)), ==, 42);
   g_value_unset (&value);
 
@@ -1241,110 +1218,6 @@ test_param_is_valid_name (void)
     g_assert_false (g_param_spec_is_valid_name (invalid_names[i]));
 }
 
-static void
-param_int_init (GParamSpec *pspec)
-{
-  GParamSpecInt *ispec = (GParamSpecInt *)pspec;
-
-  ispec->minimum = 0x7fffffff;
-  ispec->maximum = 0x80000000;
-  ispec->default_value = 0;
-}
-
-static void
-param_int_set_default (GParamSpec *pspec,
-                       GValue     *value)
-{
-  value->data[0].v_int = ((GParamSpecInt *)pspec)->default_value;
-}
-
-static gboolean
-param_int_validate (GParamSpec *pspec,
-                    GValue     *value)
-{
-  GParamSpecInt *ispec = (GParamSpecInt *)pspec;
-  int oval = value->data[0].v_int;
-
-  value->data[0].v_int = CLAMP (value->data[0].v_int, ispec->minimum, ispec->maximum);
-
-  return value->data[0].v_int != oval;
-}
-
-static int
-param_int_values_cmp (GParamSpec   *pspec,
-                      const GValue *value1,
-                      const GValue *value2)
-{
-  if (value1->data[0].v_int < value2->data[0].v_int)
-    return -1;
-  else
-    return value1->data[0].v_int > value2->data[0].v_int;
-}
-
-static GType custom_type;
-
-/* Register a pspec that has a validate vfunc, but not
- * value_is_valid, to test the fallback in g_param_value_is_valid
- */
-static void
-register_custom_pspec (void)
-{
-  const GParamSpecTypeInfo pspec_info = {
-    sizeof (GParamSpecInt),   /* instance_size */
-    16,                       /* n_preallocs */
-    param_int_init,           /* instance_init */
-    G_TYPE_INT,               /* value_type */
-    NULL,                     /* finalize */
-    param_int_set_default,    /* value_set_default */
-    param_int_validate,       /* value_validate */
-    param_int_values_cmp,     /* values_cmp */
-  };
-
-  custom_type = g_param_type_register_static ("GParamInt2", &pspec_info);
-}
-
-static GParamSpec *
-g_param_spec_custom (const char   *name,
-                     int           minimum,
-                     int           maximum,
-                     int           default_value,
-                      GParamFlags  flags)
-{
-  GParamSpecInt *ispec;
-
-  g_return_val_if_fail (default_value >= minimum && default_value <= maximum, NULL);
-
-  ispec = g_param_spec_internal (custom_type, name, NULL, NULL, flags);
-  if (ispec == NULL)
-    return NULL;
-
-  ispec->minimum = minimum;
-  ispec->maximum = maximum;
-  ispec->default_value = default_value;
-
-  return G_PARAM_SPEC (ispec);
-}
-
-static void
-test_param_spec_custom (void)
-{
-  GParamSpec *pspec;
-  GValue value = G_VALUE_INIT;
-
-  register_custom_pspec ();
-
-  pspec = g_param_spec_custom ("myint", 10, 30, 20, G_PARAM_READWRITE);
-
-  g_value_init (&value, G_TYPE_INT);
-
-  g_value_set_int (&value, 40);
-
-  g_assert_false (g_param_value_is_valid (pspec, &value));
-  g_assert_cmpint (g_value_get_int (&value), ==, 40);
-
-  g_param_spec_unref (pspec);
-}
-
 int
 main (int argc, char *argv[])
 {
@@ -1388,7 +1261,6 @@ main (int argc, char *argv[])
   g_test_add_func ("/paramspec/gtype", test_param_spec_gtype);
   g_test_add_func ("/paramspec/variant", test_param_spec_variant);
   g_test_add_func ("/paramspec/variant/cmp", test_param_spec_variant_cmp);
-  g_test_add_func ("/paramspec/custom", test_param_spec_custom);
 
   return g_test_run ();
 }
