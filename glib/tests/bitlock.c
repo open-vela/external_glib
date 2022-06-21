79505cc6d593a20e19d66719f12a33c181f1ad57
openvela-robot <openvela-robot@xiaomi.com>
2022-06-21 12:03:02 +0800

    Merge branch '2672-accel-group-crash' into 'main'
    gobject: Fix crash when disposing a GtkAccelGroup
    
    See merge request GNOME/glib!2756

--- glib/tests/bitlock.c
+++ glib/tests/bitlock.c
@@ -1,16 +1,15 @@
 #include <glib.h>
 
+#define ITERATIONS 100000000
+
 static void
 test_bitlocks (void)
 {
   guint64 start = g_get_monotonic_time ();
   gint lock = 0;
-  guint i;
-  guint n_iterations;
+  gint i;
 
-  n_iterations = g_test_perf () ? 100000000 : 1;
-
-  for (i = 0; i < n_iterations; i++)
+  for (i = 0; i < ITERATIONS; i++)
     {
       g_bit_lock (&lock, 0);
       g_bit_unlock (&lock, 0);
@@ -22,7 +21,7 @@ test_bitlocks (void)
 
     elapsed = g_get_monotonic_time () - start;
     elapsed /= 1000000;
-    rate = n_iterations / elapsed;
+    rate = ITERATIONS / elapsed;
 
     g_test_maximized_result (rate, "iterations per second");
   }
@@ -33,7 +32,8 @@ main (int argc, char **argv)
 {
   g_test_init (&argc, &argv, NULL);
 
-  g_test_add_func ("/bitlock/performance/uncontended", test_bitlocks);
+  if (g_test_perf ())
+    g_test_add_func ("/bitlock/performance/uncontended", test_bitlocks);
 
   return g_test_run ();
 }
