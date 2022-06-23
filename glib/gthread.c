0a5d6ea5aea86d81e7a851b0e0afd9d4ce000f0c
liujinye <liujinye@xiaomi.com>
2022-06-23 18:44:13 +0800

    Merge branch 'fix-macos-again' into 'main'
    tests: Fix type mismatches in new atomic tests when compiling with clang
    
    See merge request GNOME/glib!2774

--- glib/gthread.c
+++ glib/gthread.c
@@ -741,13 +741,11 @@ void
                      gsize          result)
 {
   gsize *value_location = (gsize *) location;
-  gsize old_value;
 
+  g_return_if_fail (g_atomic_pointer_get (value_location) == 0);
   g_return_if_fail (result != 0);
 
-  old_value = (gsize) g_atomic_pointer_exchange (value_location, result);
-  g_return_if_fail (old_value == 0);
-
+  g_atomic_pointer_set (value_location, result);
   g_mutex_lock (&g_once_mutex);
   g_return_if_fail (g_once_init_list != NULL);
   g_once_init_list = g_slist_remove (g_once_init_list, (void*) value_location);
