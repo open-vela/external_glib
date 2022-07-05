6a538ed56f5077d983e8137929147fd7bf098509
openvela-robot <openvela-robot@xiaomi.com>
2022-07-05 11:06:49 +0800

    Merge branch 'more-spdx' into 'main'
    Add more SPDX license headers
    
    See merge request GNOME/glib!2706

--- gobject/gobject.c
+++ gobject/gobject.c
@@ -1,6 +1,8 @@
 /* GObject - GLib Type, Object, Parameter and Signal Library
  * Copyright (C) 1998-1999, 2000-2001 Tim Janik and Red Hat, Inc.
  *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -5047,22 +5049,16 @@ g_weak_ref_set (GWeakRef *weak_ref,
       /* Remove the weak ref from the old object */
       if (old_object != NULL)
         {
-          gboolean in_weak_refs_notify;
-
           weak_locations = g_datalist_id_get_data (&old_object->qdata, quark_weak_locations);
-          in_weak_refs_notify = g_datalist_id_get_data (&old_object->qdata, quark_weak_refs) == NULL;
           /* for it to point to an object, the object must have had it added once */
-          g_assert (weak_locations != NULL || in_weak_refs_notify);
+          g_assert (weak_locations != NULL);
 
-          if (weak_locations != NULL)
+          *weak_locations = g_slist_remove (*weak_locations, weak_ref);
+
+          if (!*weak_locations)
             {
-              *weak_locations = g_slist_remove (*weak_locations, weak_ref);
-
-              if (!*weak_locations)
-                {
-                  weak_locations_free_unlocked (weak_locations);
-                  g_datalist_id_remove_no_notify (&old_object->qdata, quark_weak_locations);
-                }
+              weak_locations_free_unlocked (weak_locations);
+              g_datalist_id_remove_no_notify (&old_object->qdata, quark_weak_locations);
             }
         }
 
