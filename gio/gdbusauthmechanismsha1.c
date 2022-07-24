ab6a30fe4b499e5653c8de22f70bc8e934970e25
openvela-robot <openvela-robot@xiaomi.com>
2022-07-24 18:44:11 +0800

    Merge branch 'wip/smcv/installed-tests' into 'main'
    Fix installed-tests failures
    
    See merge request GNOME/glib!2833

--- gio/gdbusauthmechanismsha1.c
+++ gio/gdbusauthmechanismsha1.c
@@ -119,7 +119,6 @@ static gchar                   *mechanism_server_get_reject_reason  (GDBusAuthMe
 static void                     mechanism_server_shutdown           (GDBusAuthMechanism   *mechanism);
 static GDBusAuthMechanismState  mechanism_client_get_state          (GDBusAuthMechanism   *mechanism);
 static gchar                   *mechanism_client_initiate           (GDBusAuthMechanism   *mechanism,
-                                                                     GDBusConnectionFlags  conn_flags,
                                                                      gsize                *out_initial_response_len);
 static void                     mechanism_client_data_receive       (GDBusAuthMechanism   *mechanism,
                                                                      const gchar          *data,
@@ -1161,7 +1160,6 @@ mechanism_client_get_state (GDBusAuthMechanism   *mechanism)
 
 static gchar *
 mechanism_client_initiate (GDBusAuthMechanism   *mechanism,
-                           GDBusConnectionFlags  conn_flags,
                            gsize                *out_initial_response_len)
 {
   GDBusAuthMechanismSha1 *m = G_DBUS_AUTH_MECHANISM_SHA1 (mechanism);
