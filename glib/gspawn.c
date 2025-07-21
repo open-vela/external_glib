GNOME/glib

gspawn: Don’t use g_close() in async-signal-safe context
Use normal `close()` instead, which is guaranteed to be
async-signal-safe.

See `man 7 signal-safety`.

Signed-off-by: Philip Withnall <withnall@endlessm.com>

Helps: #2140

glib/gspawn.c
@@ -155,6 +155,7 @@ extern char **environ;
  */
 
 
+static gint sane_close (gint fd);
 
 static gint g_execute (const gchar  *file,
                        gchar **argv,
@@ -273,8 +274,7 @@ close_and_invalidate (gint *fd)
     return;
   else
     {
-      /* FIXME: g_close() is not async-signal-safe on failure. */
-      (void) g_close (*fd, NULL);
+      sane_close (*fd);
       *fd = -1;
     }
 }

open-vela/external_glib

glib/gspawn.c
@@ -155,6 +155,7 @@ extern char **environ;
  */
 
 
+static gint safe_close (gint fd);
 
 static gint g_execute (const gchar  *file,
                        gchar **argv,
@@ -273,8 +274,7 @@ close_and_invalidate (gint *fd)
     return;
   else
     {
-      /* FIXME: g_close() is not async-signal-safe on failure. */
-      (void) g_close (*fd, NULL);
+      safe_close (*fd);
       *fd = -1;
     }
 }